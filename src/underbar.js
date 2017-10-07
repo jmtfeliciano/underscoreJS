e53dbe5a924c52fd5c2763a849dc87749c7b756b
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-10-06 23:23:40 -0700

    deleted buggy code inside reduce, will start from scratch

--- src/underbar.js
+++ src/underbar.js
@@ -176,6 +176,7 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+      
   };
 
   // Like extend, but doesn't ever overwrite a key that already
