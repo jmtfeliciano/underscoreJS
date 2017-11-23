2828dbee4ba3dde921b753fc525b45f155927e5b
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-11-22 19:04:30 -0800

    Completed _.defaults by modifying _.extend

--- src/underbar.js
+++ src/underbar.js
@@ -208,6 +208,20 @@
   // exists in obj
   _.defaults = function(obj) {
 
+    let objects = Array.prototype.slice.call(arguments,1);
+    let combinedObject = obj;
+
+    //let falseys = [false, 0, -0, "", '', null, undefined, NaN,];
+
+    for(let i=0; i < objects.length; i++){
+      for(let key in objects[i]){
+        if(combinedObject[key] === undefined){
+          combinedObject[key] = objects[i][key]; 
+        }
+      }
+    }
+  
+    return combinedObject;
 
   };
 
